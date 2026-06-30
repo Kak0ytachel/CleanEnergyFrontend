7af65b40dc881f4b3b9b9c4623dd7c69dae68aa8
Kak0ytachel <alesha.belarus@gmail.com>
2026-07-01 00:47:18 +0200

    added api get fetch function and sample files

---
+++ src/services/api.ts
@@ -0,0 +1,9 @@
+const BASE_URL = "http://localhost:8080"; // todo: replace
+
+async function get<T>(path: string): Promise<T> {
+    const response = await fetch(`${BASE_URL}${path}`);
+    if (!response.ok) {
+        throw new Error("HTTP Error: " + response.status);
+    }
+    return response.json();
+}
