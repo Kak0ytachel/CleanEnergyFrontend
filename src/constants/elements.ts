525270ed1e27b32df65606269839d79109073463
Kak0ytachel <alesha.belarus@gmail.com>
2026-07-01 18:21:39 +0200

    PieChart additions

---
+++ src/constants/elements.ts
@@ -0,0 +1,24 @@
+
+export const ELEMENT_COLORS: Record<string, string> = {
+    "gas": "#f1a136",
+    "solar": "#F1C40F",
+    "nuclear": "#9B59B6",
+    "wind": "#1ABC9C",
+    "imports": "#7F8C8D",
+    "biomass": "#8B572A",
+    "hydro": "#2980B9",
+    "coal": "#242627",
+    "other": "#BDC3C7",
+}
+
+export const ELEMENT_ORDER: Record<string, number> = {
+    "biomass": 0,
+    "wind": 1,
+    "solar": 2,
+    "hydro": 3,
+    "nuclear": 4,
+    "gas": 5,
+    "coal": 6,
+    "imports": 7,
+    "other": 8,
+}
