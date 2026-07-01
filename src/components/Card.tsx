7900d648394a8510a18cf5dbc558cde0aa4f37d5
Kak0ytachel <alesha.belarus@gmail.com>
2026-07-01 11:04:56 +0200

    ui card component added

---
+++ src/components/Card.tsx
@@ -0,0 +1,16 @@
+import type {ReactNode} from "react";
+import styles from "./Card.module.css";
+
+
+interface ICardProps {
+    children: ReactNode;
+    className?: string;
+}
+
+export default function Card({children, className}: ICardProps) {
+    return (
+        <div className={`${styles.card} ${className}`}>
+            {children}
+        </div>
+    )
+}
