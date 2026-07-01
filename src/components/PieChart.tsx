1df96b966414b3604928812d83acd58451e0a7fe
Kak0ytachel <alesha.belarus@gmail.com>
2026-07-01 18:06:46 +0200

    PieChart fully implemented

--- src/components/PieChart.tsx
+++ src/components/PieChart.tsx
@@ -1,15 +1,43 @@
-import "./PieChart.css"
-import type {CSSProperties} from "react";
+import styles from "./PieChart.module.css";
+import type {CSSProperties, ReactElement} from "react";
+import type {IEnergyTypeItem} from "../types.ts";
+import {ELEMENT_COLORS, ELEMENT_ORDER} from "../constants/elements.ts";
 
 
-export default function PieChart() {
+interface IPieChartProps {
+    data?: IEnergyTypeItem[],
+    percentage?: number
+}
+
+
+export default function PieChart({data = [], percentage = 0}: IPieChartProps) {
+
+    data.sort((a, b) => ELEMENT_ORDER[a.fuel] - ELEMENT_ORDER[b.fuel]);
+
+    const items: ReactElement[] = [];
+    let percentageSum = 0;
+    let gradient: string = "";
+
+    for (const item of data) {
+        const perc = Math.floor(item.perc);
+        const el: ReactElement = <span
+            className={styles.pieChartLabel} key={item.fuel}
+            style={{"--start": percentageSum, "--percentage": item.perc} as CSSProperties}>{`${perc}%`}</span>
+        if (perc > 3) {
+            items.push(el);
+        }
+        const color = ELEMENT_COLORS[item.fuel];
+        gradient += `${color} ${percentageSum}% ${percentageSum + perc}%, `
+        percentageSum += perc;
+    }
+    console.log(gradient);
+    gradient = gradient.slice(0, -2); // removes trailing comma and space
     return (
         <>
-            <div className="pie-chart">
-                <span className="pie-chart-label" style={{"--start": 0, "--percentage": 1} as CSSProperties}>1%</span>
-                <span className="pie-chart-label" style={{"--start": 1, "--percentage": 69} as CSSProperties}>69%</span>
-                <span className="pie-chart-label" style={{"--start": 70, "--percentage": 30} as CSSProperties}>30%</span>
-            </div>
+            <div className={styles.pieChart} style={{backgroundImage: `conic-gradient(${gradient})`} as CSSProperties}>
+                <div className={styles.pieChartArc} style={{"--percentage": `${percentage}%`} as CSSProperties}/>
+                {items}
+           </div>
         </>
     )
 }
